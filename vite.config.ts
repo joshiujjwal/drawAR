ab6163ad1cb57b8b201aed7bfbcb9bb105f13977
Ujjwal Joshi <ujjwaljoshi93@gmail.com>
2024-03-29 16:53:50 -0400

    update express

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ export default defineConfig({
   server: {
     proxy: {
       '/uploadFile': {
-        target: 'http://localhost:3000',
+        target: 'https://draw-ar-express.vercel.app',
         changeOrigin: true,
       },
       '/assets': {
-        target: 'http://localhost:3000',
+        target: 'https://draw-ar-express.vercel.app',
         changeOrigin: true,
       },
       '/getFiles': {
-        target: 'http://localhost:3000',
+        target: 'https://draw-ar-express.vercel.app',
         changeOrigin: true,
       },
     },
