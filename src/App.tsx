61472160325a20396910e3f3548cd89a4f9080a7
Ujjwal Joshi <ujjwaljoshi93@gmail.com>
2024-03-29 04:53:51 -0400

    remove upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Upload from './components/upload/Upload';
+// import Upload from './components/upload/Upload';
 import XrHitCubeContainer from './components/xr-hit-cube/XrHitCubeContainer'
 import Cube from './components/cube/Cube';
 import XrCube from './components/xr-cube/XrCube';
@@ -14,7 +14,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/cube"  element={<Cube />}  />
       <Route path="/xr-cube" element={<XrCube />} />
-      <Route path="/upload" element={<Upload />} />
+      <Route path="/upload" element={<h1> Upload </h1>} />
       <Route path="/view" element={<XrHitCubeContainer />} />
     </Routes>
   )
--- src/components/upload/Upload.tsx
+++ src/components/upload/Upload.tsx
@@ -1,41 +1,40 @@
-import React, { ChangeEvent, useState } from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
-import DrawNavbar from '../drawnavbar/DrawNavbar';
+// import React, { ChangeEvent, useState } from 'react';
+// import { Button, Container, Form } from 'react-bootstrap';
+// import DrawNavbar from '../drawnavbar/DrawNavbar';
 
-const Upload: React.FC = () => {
-    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+// const Upload: React.FC = () => {
+//     const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        setSelectedFile(file || null);
-    };
+//     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+//         const file = event.target.files?.[0];
+//         setSelectedFile(file || null);
+//     };
 
-    const handleUpload = () => {
-        if (selectedFile) {
-            const allowedExtensions = ['.nwd', '.obj'];
-            const fileExtension = selectedFile.name.split('.').pop()?.toLowerCase();
+//     const handleUpload = () => {
+//         if (selectedFile) {
+//             const allowedExtensions = ['.nwd', '.obj'];
+//             const fileExtension = selectedFile.name.split('.').pop()?.toLowerCase();
 
-            if (allowedExtensions.includes(`.${fileExtension}`)) {
-                // Save the file to the assets upload folder
+//             if (allowedExtensions.includes(`.${fileExtension}`)) {
+//                 // Save the file to the assets upload folder
 
+//                 alert('File uploaded successfully!');
+//             } else {
+//                 alert('Invalid file format. Only NWD and OBJ files are allowed.');
+//             }
+//         }
+//     };
+//     return (
+//         <Container>
+//             <DrawNavbar />
+//             <Container className="d-flex flex-column align-items-center justify-content-center">
+//                 <Form.Group controlId="formFile" className="mb-3"> 
+//                     <Form.Control type="file" onChange={handleFileChange} placeholder='Only nwd or obj'/>
+//                 </Form.Group>
+//                 <Button variant="primary" onClick={handleUpload}>Upload</Button>
+//             </Container>
+//         </Container>
+//     );
+// };
 
-                alert('File uploaded successfully!');
-            } else {
-                alert('Invalid file format. Only NWD and OBJ files are allowed.');
-            }
-        }
-    };
-    return (
-        <Container>
-            <DrawNavbar />
-            <Container className="d-flex flex-column align-items-center justify-content-center">
-                <Form.Group controlId="formFile" className="mb-3">
-                    <Form.Control type="file" onChange={handleFileChange} placeholder='Only nwd or obj' />
-                </Form.Group>
-                <Button variant="primary" onClick={handleUpload}>Upload</Button>
-            </Container>
-        </Container>
-    );
-};
-
-export default Upload;
+// export default Upload;
